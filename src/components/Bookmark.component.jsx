047d951c4e4252599b00211b399069532422aba5
dimonabinsk <dimonabinsk@yandex.ru>
2022-09-19 19:21:26 +0300

    произвел форматирование всего проекта prattierr

--- src/components/Bookmark.component.jsx
+++ src/components/Bookmark.component.jsx
@@ -5,13 +5,11 @@ const Bookmark = ({ id, onHandler, mark }) => {
     return (
         <>
             <button className="btn btn-sm" onClick={() => onHandler(id)}>
-                {mark
-                    ? (
-                        <i className="bi bi-emoji-heart-eyes-fill fs-2 text-primary"></i>
-                    )
-                    : (
-                        <i className="bi bi-emoji-heart-eyes fs-2"></i>
-                    )}
+                {mark ? (
+                    <i className="bi bi-emoji-heart-eyes-fill fs-2 text-primary"></i>
+                ) : (
+                    <i className="bi bi-emoji-heart-eyes fs-2"></i>
+                )}
             </button>
         </>
     );
