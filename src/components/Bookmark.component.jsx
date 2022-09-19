937d7dfad06b4ac71a0b5af6e50b70748fff81b2
dimonabinsk <dimonabinsk@yandex.ru>
2022-09-19 19:29:56 +0300

    fix

--- src/components/Bookmark.component.jsx
+++ src/components/Bookmark.component.jsx
@@ -5,11 +5,13 @@ const Bookmark = ({ id, onHandler, mark }) => {
     return (
         <>
             <button className="btn btn-sm" onClick={() => onHandler(id)}>
-                {mark ? (
-                    <i className="bi bi-emoji-heart-eyes-fill fs-2 text-primary"></i>
-                ) : (
-                    <i className="bi bi-emoji-heart-eyes fs-2"></i>
-                )}
+                {mark
+                    ? (
+                        <i className="bi bi-emoji-heart-eyes-fill fs-2 text-primary"></i>
+                    )
+                    : (
+                        <i className="bi bi-emoji-heart-eyes fs-2"></i>
+                    )}
             </button>
         </>
     );
