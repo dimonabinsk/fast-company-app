b93184d488f95b977f907cb372a307d836d05a2d
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-01-30 21:37:54 +0300

    Настройка рабочего окружения

--- server/app.js
+++ server/app.js
@@ -6,6 +6,12 @@ const chalk = require("chalk");
 const PORT = config.get("port") ?? 8080;
 const app = express();
 
+// if (process.env.NODE_ENV === "production") {
+// console.log("Production");
+// } else {
+//     console.log("Development");
+// }
+
 app.listen(PORT, () => {
   console.log(chalk.green(`Сервер запущен на порте: ${PORT}`));
 });
