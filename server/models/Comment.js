13daacb4c6974806d0d68da9f5bfda6c141d5333
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-02-01 10:56:22 +0300

    Создаем модели

---
+++ server/models/Comment.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const schema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    pageId: {
+      //   На чьей странице находится комментарий
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    userId: {
+      //   Кто оставил комментарий
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+    },
+  }
+);
+
+module.exports = model("Comment", schema);
