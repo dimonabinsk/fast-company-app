7a200b56615636971c2073c0d351a71825a6c5e4
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-02-01 15:45:16 +0300

    добавил .env

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
 const initDataBase = require("./startUp/initDataBase");
 const routes = require("./routes");
-
 
 const app = express();
 app.use(express.json());
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 // /api
 app.use("/api", routes);
 const PORT = config.get("port") ?? 8080;
+const MongoDB = process.env.DB_MONGO;
+
 // if (process.env.NODE_ENV === "production") {
 // console.log("Production");
 // } else {
@@ -24,7 +26,7 @@ async function start() {
     mongoose.connection.once("open", () => {
       initDataBase();
     });
-    await mongoose.connect(config.get("mongoUri"));
+    await mongoose.connect(MongoDB);
     console.log(chalk.green("MongoDB подключен!"));
     app.listen(PORT, () => {
       console.log(
