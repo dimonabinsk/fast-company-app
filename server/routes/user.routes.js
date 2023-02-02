dd1f06667350ee2af71397483450f57ae57f35b9
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-02-02 11:06:11 +0300

    Создаем Middleware для авторизации

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const User = require("../models/User");
+const auth = require("../middleware/auth.middleware");
 const router = express.Router({ mergeParams: true });
 
-router.patch("/:userId", async (req, res) => {
+router.patch("/:userId", auth, async (req, res) => {
   try {
     const { userId } = req.params;
     // todo: userId === current user id
-    if (userId) {
+    // console.log(req.user);
+    if (userId === req.user._id) {
       const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
         new: true,
       });
@@ -21,7 +23,7 @@ router.patch("/:userId", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const list = await User.find();
 
