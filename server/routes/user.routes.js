c90a6516c8aedcdac6e115eeb83e497246112524
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-02-02 10:31:18 +0300

    Пишем логику для работы с пользователями

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,35 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
 
+router.patch("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    // todo: userId === current user id
+    if (userId) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+      res.status(200).send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      massage: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
 
+router.get("/", async (req, res) => {
+  try {
+    const list = await User.find();
+
+    res.status(200).send(list);
+  } catch (error) {
+    res.status(500).json({
+      massage: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
 module.exports = router;
